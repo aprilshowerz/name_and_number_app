5ffd7443b243555b9d6adaf7aeb74cab2757efa1
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-19 22:34:22 -0500

    added age erb

--- app.rb
+++ app.rb
@@ -11,6 +11,14 @@ post '/name' do
 
 	user_name = params[:name_input]
 
-	erb :greeting, :locals => {:user_name => user_name}
+	#erb :greeting, :locals => {:user_name => user_name}
+	erb :age_input, :locals => {:user_name => user_name}
+
+post '/age' do	
+	user_name = params[:name_input]
+	user_age = params[:age_input]
+	erb :favorite_numbers, :locals => {:user_name => user_name, :age => user_age}
+end
+
 
 end
