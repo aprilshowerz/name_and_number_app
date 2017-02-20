4422ff3c2d2f0ac9958c26998338a09f124f35b4
April Blair <a.m.blair@eagle.clarion.edu>
2017-02-20 10:22:17 -0500

    finally working!

--- app.rb
+++ app.rb
@@ -9,16 +9,52 @@ end
 
 post '/name' do
 
-	user_name = params[:name_input]
+	backend_name = params[:name_input]
 
 	#erb :greeting, :locals => {:user_name => user_name}
-	erb :age_input, :locals => {:user_name => user_name}
-
-post '/age' do	
-	user_name = params[:name_input]
-	user_age = params[:age_input]
-	erb :favorite_numbers, :locals => {:user_name => user_name, :age => user_age}
+	erb :age, :locals => {:user_name => backend_name}
 end
 
+post '/user_age' do	
 
+	backend_name = params[:name_input]
+	backend_age = params[:age_input]
+
+	erb :favorite_nums, :locals => {:user_name => backend_name, :age => backend_age}
 end
+
+post '/favorite_nums' do
+	backend_name = params[:name_input]
+	backend_age = params[:age_input].to_i
+	backend_num_1 = params[:fav_input_1].to_i
+	backend_num_2 = params[:fav_input_2].to_i
+	backend_num_3 = params[:fav_input_3].to_i
+	sum = (backend_num_1 + backend_num_2 + backend_num_3)
+
+	results = 'less than'
+		if (sum > backend_age)
+				results = 'greater than'
+			elsif (sum == backend_age)
+				results = 'equal to'
+			else
+				results
+		end
+erb :final_feedback, :locals => {:user_name => backend_name, :age => backend_age, 
+	:fav_num_1 => backend_num_1, :fav_num_2 => backend_num_2, :fav_num_3 => backend_num_3,
+	 :sum => sum, :results => results}
+end
+
+post 'final_feedback' do
+	backend_name = params[:name_input]
+	backend_age = params[:age_input].to_i
+	backend_num_1 = params[:fav_input_1].to_i
+	backend_num_2 = params[:fav_input_2].to_i
+	backend_num_3 = params[:fav_input_3].to_i
+
+erb :final_feedback, :locals => {:user_name => backend_name, :age => backend_age, 
+	:fav_num_1 => backend_num_1, :fav_num_2 => backend_num_2, :fav_num_3 => backend_num_3,
+	 :sum => sum, :results => results}
+end
+
+				
+
